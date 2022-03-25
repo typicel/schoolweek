7cdd75322da873721768b50f2ad970bd14dc87a9
Ty <42102494+Endatronix@users.noreply.github.com>
2022-03-24 23:40:43 -0600

    relative time now shows on tasks, editing notes now fixed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,13 +5,17 @@ import MDEditor from "@uiw/react-md-editor";
 const TodoForm = ({ addTask, theme }) => {
   const [task, setTask] = useState("");
   const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
   const [notes, setNotes] = useState("");
 
-  const checkDate = (due) => {
-    let dateObj = new Date(due + " 00:00");
+  const checkDate = (due, time) => {
+    if (due === "" && time !== "") return -1; //User shouldn't be able to enter a time without a date
+
+    let formatDate = time === "" ? due : due + "T" + time;
+
+    let dateObj = new Date(formatDate);
     let today = new Date();
-    let timeLeft = (dateObj.getTime() - today.getTime()) / 1000;
-    timeLeft = Math.ceil(timeLeft / 86400);
+    let timeLeft = Math.ceil((dateObj.getTime() - today.getTime()) / 86400000);
 
     let result = timeLeft < 0 ? -1 : 0;
     return result;
@@ -20,19 +24,17 @@ const TodoForm = ({ addTask, theme }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (checkDate(date) === -1) {
+    if (checkDate(date, time) === -1) {
       alert("Please enter a valid date");
-      return;
     } else if (task.length <= 0) {
       console.log("huh");
       alert("Please enter a task name");
-      return;
     } else {
-      console.log("wtf");
-      addTask(task, date, notes);
+      addTask(task, date, time, notes);
       setTask("");
       setNotes("");
       setDate("");
+      setTime("");
     }
   };
 
@@ -41,21 +43,25 @@ const TodoForm = ({ addTask, theme }) => {
   return (
     <div className="d-flex justify-content-center m-3">
       <Form className="form-styles" onSubmit={handleSubmit}>
+        <Form.Control
+          className="dark-input my-3"
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="What needs to be done?"
+        />
         <div className="d-flex display-inline">
           <Form.Control
             className="dark-input"
-            style={{ width: "20rem" }}
-            type="text"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            placeholder="What needs to be done?"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
           />
           <Form.Control
             className="dark-input"
-            type="date"
-            style={{ width: "16rem" }}
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+            type="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
           />
         </div>
 
