bcef042e92957222c759568a8b47b7d63337731e
Tyler McCormick <tyler.mccormick@colorado.edu>
2024-05-26 22:20:37 -0600

    firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYNsokq9Y210zG0QiqaiMt7fNkxrm6vgc",
-  authDomain: "schoolweek.firebaseapp.com",
-  projectId: "schoolweek",
-  storageBucket: "schoolweek.appspot.com",
-  messagingSenderId: "452246359895",
-  appId: "1:452246359895:web:edf63ce6abfdd7b737f812",
-  measurementId: "G-G08PNENKCP",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
